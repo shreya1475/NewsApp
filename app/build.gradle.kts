1fdc7cef7227060f93ea844c7f205ddb4800d659
shreya1475 <shindess23.comp@coeptech.ac.in>
2025-07-09 12:44:11 +0530

    Post Editor backend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,5 +64,15 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    // Retrofit + OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+// Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+
+// Activity Result (safe)
+    implementation("androidx.activity:activity-ktx:1.8.2")
+
 
 }
